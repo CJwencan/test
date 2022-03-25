c62851b000be2d5147cf36e0664730663d4a4ae5
wcn0743 <wcn0743@corp.netease.com>
2022-03-25 10:50:41 +0800

    Trying to commit Git

---
+++ commom.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source ${GDCJOBBASE}/etl/_bin/common.cfg
+
+#调度器开始日期
+export DATA_DATE=$1
+FILE_NAME=$0
+
+#获取第一天
+function get_FirstDate(){
+    echo ${1:0:6}01
+}
+
+#获取前或后 N 天    # get_date 20140815 -1
+function get_date(){
+    echo `date -d "${1%%-*} ${2} days" "+%Y%m%d"`
+}
